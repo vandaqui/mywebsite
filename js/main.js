7cca3d6607e23eb2e363df01971059d236deabf7
Vand Aquino <64815489+vandaqui@users.noreply.github.com>
2021-06-10 11:20:52 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log
+/*--------- ABOUT SECTION ---------*/
+
+(() =>{
+  const aboutSection = document.querySelector(".about-section"),
+        tabsContainer = documento.querySelector(".about-tabs");
+  /* if event.target contains 'tab-item' class and not contains 'active' class */
+  tabsContainer.addEventListener("click", (event) => {
+    if(event.target.classList.contains("tab-item") && !event.target.classList.contains("active")){
+    const target = event.target.getAttribute("data-target");
+    // deactivate existing active 'tab item'
+    tabsContainer.querySelector(".active").classList.remove("outer-shadow", "active");
+    // activate new 'tab-item'
+    event.target.classList.add("active", "outer-shadow");
+    }
+  })
+})();
