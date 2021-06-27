d5f60817cc93d4681f578416edf18e8ee4ab88ac
vandicleydson@gmail.com <vandicleydson@gmail.com>
2021-06-26 22:03:57 -0300

    Fix animations, Update JS

--- js/main.js
+++ js/main.js
@@ -227,3 +227,14 @@ function bodyScrollingToggle(){
     slider();
 
 })();
+
+/*--------- HIDE ALL SECTIONS EXPECT ACTIVE---------*/
+
+(() =>{
+  const sections = document.querySelectorAll(".section");
+  sections.forEach((section)=>{
+    if(!section.classList.contains("active")){
+      section.classList.add("hide");
+    }
+  })
+})();
