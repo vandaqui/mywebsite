ee90a3dfb8ea6622d8c91d061a62c14086d45842
vandicleydson@gmail.com <vandicleydson@gmail.com>
2021-06-27 17:25:14 -0300

    Add preloader JS

--- js/main.js
+++ js/main.js
@@ -307,3 +307,12 @@ function bodyScrollingToggle(){
     }
   })
 })();
+
+
+window.addEventListener("load", () =>{
+  // preloader
+  document.querySelector(".preloader").classList.add("fadeout");
+  setTimeout(()=>{
+    document.querySelector(".preloader").style.display="none";
+  }, 600)
+})
