c9b92cfc6a43628c88733397ca475015cb1bcf8f
Vandaqui-windows-main <vandicleydson@gmail.com>
2021-10-14 22:08:40 -0300

    Remove testimonial js

--- js/main.js
+++ js/main.js
@@ -247,54 +247,6 @@ function bodyScrollingToggle(){
 
     */})();
 
-/*--------- TESTIMONIAL SLIDER ---------*/
-(() =>{
-    const sliderContainer = document.querySelector(".testi-slider-container"),
-    slides = sliderContainer.querySelectorAll(".testi-item"),
-    slideWidth = sliderContainer.offsetWidth,
-    prevBtn = document.querySelector(".testi-slider-nav .prev"),
-    nextBtn = document.querySelector(".testi-slider-nav .next"),
-    activeSlide = sliderContainer.querySelector(".testi-item.active");
-    let slideIndex = Array.from(activeSlide.parentElement.children).indexOf(activeSlide);
-
-    //set width of all slides
-    slides.forEach((slide) =>{
-      slide.style.width = slideWidth + "px";
-    })
-    //set width  of sliderContainer
-    sliderContainer.style.width = slideWidth * slides.length + "px";
-
-    nextBtn.addEventListener("click", () =>{
-      if(slideIndex === slides.length-1){
-        slideIndex = 0;
-      }
-      else{
-        slideIndex++;
-      }
-      sliderContainer.style.marginLeft = - (slideWidth * slideIndex) + "px";
-    })
-
-    prevBtn.addEventListener("click", () => {
-      if(slideIndex === 0){
-        slideIndex = slides.length-1;
-      }
-      else{
-        slideIndex--;
-      }
-      slider();
-    })
-
-    function slider(){
-      // deactivate existing active slide
-      sliderContainer.querySelector(".testi-item.active").classList.remove("active");
-      // activate new slide
-      slides[slideIndex].classList.add("active");
-      sliderContainer.style.marginLeft = - (slideWidth * slideIndex) + "px";
-    }
-    slider();
-
-})();
-
 (() =>{
   const sections = document.querySelectorAll(".section");
   sections.forEach((section)=>{
