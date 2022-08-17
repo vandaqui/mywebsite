16d2bcce47776b330ab5a19255a204fd40fa4a99
Vand Aquino <vandicleydson@gmail.com>
2022-08-16 23:16:49 -0300

    layout overhaul

--- js/main-old.js
+++ js/main-old.js
@@ -1,6 +1,27 @@
 
 /*--------- NAVIGATION MENU ---------*/
 (()=>{
+  const hamburgerBtn = document.querySelector(".hamburger-btn"),
+  navMenu = document.querySelector(".nav-menu"),
+  closeNavBtn = navMenu.querySelector(".close-nav-menu");
+  hamburgerBtn.addEventListener("click", showNavMenu);
+  closeNavBtn.addEventListener("click", hideNavMenu);
+  
+  function showNavMenu(){
+    navMenu.classList.add("open");
+    bodyScrollingToggle();
+  }
+  function hideNavMenu(){
+    navMenu.classList.remove("open");
+    fadeOutEffect();
+    bodyScrollingToggle();
+  }
+  function fadeOutEffect(){
+    document.querySelector(".fade-out-effect").classList.add("active");
+    setTimeout(() =>{
+      document.querySelector(".fade-out-effect").classList.remove("active");
+    },300)
+  }
   // attach and event handler to document
   document.addEventListener("click", (event) =>{
     if(event.target.classList.contains('link-item')){
@@ -18,12 +39,33 @@
         // deactivate existing active navigation menu
         navMenu.querySelector(".active").classList.add("outer-shadow","hover-in-shadow");
         navMenu.querySelector(".active").classList.remove("active","inner-shadow");
+        /* if clicked 'link-item' is contained whitin the navigation menu */
+        if (navMenu.classList.contains("open")){
+        // activate new navigation menu 'link-item'
+        event.target.classList.add("active","inner-shadow");
+        event.target.classList.remove("outer-shadow","hover-in-shadow");
+        // hide navtigation menu
+        hideNavMenu();
+      }
+      else{
+        let navItems = navMenu.querySelectorAll(".link-item");
+        navItems.forEach((item) =>{
+          if(hash === item.hash){
+            // activate new navigation menu 'link-item'
+            item.classList.add("active", "inner-shadow");
+            item.classList.remove("outer-shadow", "hover-in-shadow");
+        }
+      })
+      fadeOutEffect();
+      }
       // add hash(#) to url
       window.location.hash = hash;
       }
     }
   })
+
 })();
+
 
 /*--------- ABOUT SECTION ---------*/
 (() =>{
@@ -154,6 +196,15 @@ function bodyScrollingToggle(){
         popup.querySelector(".pp-title h2").innerHTML = title;
       }
 })();
+
+/*(() =>{
+  const sections = document.querySelectorAll(".section");
+  sections.forEach((section)=>{
+    if(!section.classList.contains("active")){
+     section.classList.add("hide");
+    }
+  })
+})();*/
 
 window.addEventListener("load", () =>{
   // preloader
